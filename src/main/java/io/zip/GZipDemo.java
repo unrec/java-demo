0bb810e1443841ed2913ae9285c3e68ffaf7e9cb
unrec <andrei.troitskiy@gmail.com>
2020-08-26 12:40:34 +0300

    GZipDemo added

---
+++ src/main/java/io/zip/GZipDemo.java
@@ -0,0 +1,38 @@
+package io.zip;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.zip.GZIPOutputStream;
+
+public class GZipDemo {
+    public static void main(String[] args) throws IOException {
+
+        File sourceFile = new File("src/main/resources/sample.txt");
+        File outputFile = new File("src/main/resources/sample.txt.gz");
+
+        try (FileInputStream fileInputStream = new FileInputStream(sourceFile);
+             FileOutputStream fileOutputStream = new FileOutputStream(outputFile);
+             GZIPOutputStream gzipOutputStream = new GZIPOutputStream(fileOutputStream)) {
+            gzipOutputStream.write(fileInputStream.readAllBytes());
+        }
+
+        //or
+//        compressGzip(sourceFile.toPath(), outputFile.toPath());
+    }
+
+    public static void compressGzip(Path source, Path target) throws IOException {
+        try (GZIPOutputStream gos = new GZIPOutputStream(
+                new FileOutputStream(target.toFile()));
+             FileInputStream fis = new FileInputStream(source.toFile())) {
+
+            byte[] buffer = new byte[1024];
+            int len;
+            while ((len = fis.read(buffer)) > 0) {
+                gos.write(buffer, 0, len);
+            }
+        }
+    }
+}
