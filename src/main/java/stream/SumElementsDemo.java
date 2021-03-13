ad5ec0389e479238a13c1528f57525e71953417f
unrec <andrei.troitskiy@gmail.com>
2021-03-13 22:25:58 +0300

    SumElementsDemo updated

--- src/main/java/stream/SumElementsDemo.java
+++ src/main/java/stream/SumElementsDemo.java
@@ -1,5 +1,8 @@
 package stream;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
 public class SumElementsDemo {
@@ -8,13 +11,20 @@ public class SumElementsDemo {
                 new Product().setCount(10).setWeight(5),
                 new Product().setCount(5).setWeight(2),
                 new Product().setCount(100).setWeight(1),
-                new Product().setCount(25).setWeight(4)
-        );
+                new Product().setCount(25).setWeight(4));
+
         int getOrderWeight = products.stream()
                 .map(product -> product.getWeight() * product.getCount())
                 .mapToInt(Integer::intValue)
                 .sum();
 
         System.out.println("getOrderWeight = " + getOrderWeight);
+    }
+
+    @Data
+    @NoArgsConstructor
+    private static class Product {
+        private Integer weight;
+        private Integer count;
     }
 }
